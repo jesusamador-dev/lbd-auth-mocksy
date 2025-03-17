3bcf89359c29e79ea936effe19e406b52bb64bf2
Jesus Macias <jesus.macias.amador@gmail.com>
2025-03-17 12:15:52 -0600

    feat(auth): se agrega el gateway de auth con cognito

---
+++ src/domain/interfaces/gateways/auth_gateway_interface.py
@@ -0,0 +1,19 @@
+from abc import ABC, abstractmethod
+
+
+class AuthGatewayInterface(ABC):
+    @abstractmethod
+    def sign_up(self, email: str, password: str):
+        pass
+
+    @abstractmethod
+    def sign_in(self, email: str, password: str):
+        pass
+
+    @abstractmethod
+    def refresh_token(self, refresh_token: str):
+        pass
+
+    @abstractmethod
+    def authorizer(self, access_token: str):
+        pass
