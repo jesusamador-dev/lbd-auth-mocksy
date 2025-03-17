aa42e29f96c03e0d90ac7281fa8d80d2f23c9492
Jesus Macias <jesus.macias.amador@gmail.com>
2025-03-17 10:16:16 -0600

    chore(config): se configura terraform correctamente

--- infra/bash/check_lambda.sh
+++ infra/bash/check_lambda.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 set -e  # Detiene el script si hay un error
 
-# Leer JSON desde Terraform
-read input_json || { echo '{"exists": false}'; exit 1; }
-
-# Extraer la variable `lambda_name`
-LAMBDA_NAME=$(echo "$input_json" | jq -r '.lambda_name')
+eval "$(jq -r '@sh "LAMBDA_NAME=\(.lambda_name)"')"
 
 # Si la variable está vacía, devolver error
 if [[ -z "$LAMBDA_NAME" ]]; then
   echo "❌ Error: 'lambda_name' no definido"
-  echo '{"exists": false}'
+  echo '{"exists": "false"}'
   exit 1
 fi
 
-# Verificar si la Lambda existe
-if aws lambda get-function --function-name "$LAMBDA_NAME" >/dev/null 2>&1; then
-  echo '{"exists": true}'
+# Verificar si la Lambda existe y extraer su ARN
+LAMBDA_ARN=$(aws lambda get-function --function-name "$LAMBDA_NAME" --query 'Configuration.FunctionArn' --output text 2>/dev/null || echo "")
+
+if [[ -n "$LAMBDA_ARN" ]]; then
+  echo "{\"exists\": \"true\", \"arn\": \"$LAMBDA_ARN\"}"
 else
-  echo '{"exists": false}'
+  echo '{"exists": "false", "arn": ""}'
 fi
+
+
